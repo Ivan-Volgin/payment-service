c69460456c9a5ab103c1b145e928c636ba7e05dc
Ivan Volgin <volgin.ivan.05@gmail.com>
2025-01-14 22:56:06 +0300

    added deposit func to pgdb

--- internal/repo/pgdb/user.go
+++ internal/repo/pgdb/user.go
@@ -9,7 +9,7 @@ import (
 	"errors"
 	// "log"
 
-	// "github.com/Masterminds/squirrel"
+	"github.com/Masterminds/squirrel"
 	"github.com/jackc/pgx/v5"
 	// "github.com/jackc/pgx/v5/pgconn"
 )
@@ -24,7 +24,7 @@ func NewUserRepo(pg *postgres.Postgres) *UserRepo{
 
 func (u *UserRepo) GetBalanceByUUID(ctx context.Context, uuid string) (entity.User, error) {
 
-	sql, args, _ := u.Builder.Select("*").From("users").Where("id = ?", uuid).ToSql()
+	sql, args, _ := u.Builder.Select("*").From("users").Where("uuid = ?", uuid).ToSql()
 
 	var user entity.User
 	err := u.Pool.QueryRow(ctx, sql, args...).Scan(
@@ -38,4 +38,30 @@ func (u *UserRepo) GetBalanceByUUID(ctx context.Context, uuid string) (entity.Us
 		return entity.User{}, fmt.Errorf("AccountRepo.GetAccountById - r.Pool.QueryRow: %v", err)
 	}
 	return user, nil
+}
+
+func (u *UserRepo) Deposit(ctx context.Context, uuid string, amount uint64) error {
+	tx, err := u.Pool.Begin(ctx)
+	if err != nil {
+		return fmt.Errorf("AccountRepo.Deposit - r.Pool.Begin: %v", err)
+	}
+	defer func() { _ = tx.Rollback(ctx) }()
+
+	sql, args, _ := u.Builder.
+		Update("users").
+		Set("balance", squirrel.Expr("balance + ?", amount)).
+		Where("uuid = ?", uuid).
+		ToSql()
+
+	_, err = tx.Exec(ctx, sql, args...)
+	if err != nil {
+		return fmt.Errorf("AccountRepo.Deposit - tx.Exec: %v", err)
+	}
+
+	err = tx.Commit(ctx)
+	if err != nil {
+		return fmt.Errorf("AccountRepo.Deposit - tx.Commit: %v", err)
+	}
+	
+	return nil
 }
